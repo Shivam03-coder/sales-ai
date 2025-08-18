d301e2fa2685e9af47ef00b8064d4072a6b9cd71
Shivam03-coder <shivam850anand@gmail.com>
2025-08-19 01:36:42 +0530

    widget session id added to codebase

--- packages/server/convex/_generated/api.d.ts
+++ packages/server/convex/_generated/api.d.ts
@@ -13,6 +13,7 @@ import type {
   FilterApi,
   FunctionReference,
 } from "convex/server";
+import type * as public_contact_session from "../public/contact-session.js";
 
 /**
  * A utility for referencing Convex functions in your app's API.
@@ -22,7 +23,9 @@ import type {
  * const myFunctionReference = api.myModule.myFunction;
  * ```
  */
-declare const fullApi: ApiFromModules<{}>;
+declare const fullApi: ApiFromModules<{
+  "public/contact-session": typeof public_contact_session;
+}>;
 export declare const api: FilterApi<
   typeof fullApi,
   FunctionReference<any, "public">
